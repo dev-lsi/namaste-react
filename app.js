fe895e0c3f3876024419b8df7374eaffe364e011
LachezarIvanov-git <lachezar.ivanov.dev@gmail.com>
2024-03-10 03:08:37 +0200

    dist and parcel-cache added to gitignore

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
 
 const reactHeading=React.createElement("h2", 
 {
@@ -5,22 +7,7 @@ const reactHeading=React.createElement("h2",
 }, 
 "Hello world by React");
 
-const reactHeading2=React.createElement("h2", 
-{
-  id:"react-heading2"
-}, 
-"Hello world by React in root 2");
 
-const reactHeading3=React.createElement("h2", 
-{
-  id:"react-heading3"
-}, 
-"Hello world by React in root2 changed after 3 seconds");
+const reactRoot= ReactDOM.createRoot(document.getElementById('react-root'));
 
-const reactRoot=ReactDOM.createRoot(document.getElementById('react-root'));
 reactRoot.render(reactHeading);
-
-const reactRoot2=ReactDOM.createRoot(document.getElementById('react-root2'));
-reactRoot2.render(reactHeading2);
-
-setTimeout(()=>reactRoot2.render(reactHeading3),3000)
