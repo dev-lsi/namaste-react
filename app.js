73c4e0b05755a09c391f1feb230b799faf7d570c
LachezarIvanov-git <lachezar.ivanov.dev@gmail.com>
2024-03-10 22:57:39 +0200

    play with jsx

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-const reactHeading=React.createElement("h2", 
-{
-  id:"react-heading"
-}, 
-"Hello world by React");
+const heading =React.createElement("h1",{id:"heading"},"Hello from React!");
+console.log(heading);
 
 
-const reactRoot= ReactDOM.createRoot(document.getElementById('react-root'));
+const Title=()=>(<h1 id="jsxHeading">JSX Title from JSX </h1>);
+const Counter=(c)=><p>Counter paragraph</p>;
 
-reactRoot.render(reactHeading);
+const HeadingComponent=()=>(
+  <div id="container">
+    <Counter/>
+    <Title/>
+    <h1 className="heading">Header from functional React Component</h1>
+  </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(HeadingComponent());
