e0ce5a09c511a4f3c000c964069174e52b165769
LachezarIvanov-git <lachezar.ivanov.dev@gmail.com>
2024-03-30 21:09:45 +0200

    init commit of version 2.0

---
+++ App.js
@@ -0,0 +1,42 @@
+
+function app() {
+
+  const documentRoot = document.getElementById("root");
+  const reactRoot=document.getElementById("react-root");
+  const heading = document.createElement("h1");
+  heading.textContent = "Hello world by JS";
+
+  let counter = 0;
+
+    while (counter <= 5) {
+      
+        counter++;
+        
+        function printCounter(x){
+
+            setTimeout(() => {
+        
+                heading.textContent = `Hello world by JS  + ${x}`;
+                documentRoot.append(heading);
+              }, counter * 2000);
+
+        }
+        
+        printCounter(counter);
+
+        const li1=React.createElement('li',{},"List Item 1");
+        const li2=React.createElement('li',{},"List Item 2");
+        const li3=React.createElement('li',{},"List Item 3");
+
+
+        const reactHeading=React.createElement('ul',{id:'heading'},"List Title",li1,li2,li3);
+        const root = ReactDOM.createRoot(reactRoot);
+        root.render(reactHeading);
+
+        console.log(reactHeading)
+
+    }
+  
+}
+
+app();
