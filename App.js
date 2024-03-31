808d5ace19abb206f531906fcde99cd50e530563
LachezarIvanov-git <lachezar.ivanov.dev@gmail.com>
2024-03-31 11:53:41 +0300

    using components

--- App.js
+++ App.js
@@ -1,43 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-function app() {
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-  const documentRoot = document.getElementById("root");
-  const reactRoot=document.getElementById("react-root");
-  const heading = document.createElement("h1");
-  heading.textContent = "Hello world by JS";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-  let counter = 0;
+const title = (<h1>Namaste React</h1>);
 
-    while (counter <= 5) {
-      
-        counter++;
-        
-        function printCounter(x){
-
-            setTimeout(() => {
-        
-                heading.textContent = `Hello world by JS  + ${x}`;
-                documentRoot.append(heading);
-              }, counter * 2000);
-
-        }
-        
-        printCounter(counter);
-
-        const li1=React.createElement('li',{},"List Item 1");
-        const li2=React.createElement('li',{},"List Item 2");
-        const li3=React.createElement('li',{},"List Item 3");
-
-
-        const reactHeading=React.createElement('ul',{id:'heading'},"List Title",li1,li2,li3);
-        const root = ReactDOM.createRoot(reactRoot);
-        root.render(reactHeading);
-
-        console.log(reactHeading)
-
-    }
-  
-}
-
-app();
+const Heading = () => (
+  <div>
+    {title}
+    <h2>by Akshay Asani</h2>
+  </div>
+);
+root.render(<Heading />);
